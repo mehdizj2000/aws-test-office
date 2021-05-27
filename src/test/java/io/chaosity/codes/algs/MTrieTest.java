55920e461cd53451004b0727abfa2e767b292e78
mehdizj2000 <zareimeh@gmail.com>
2021-05-27 22:26:57 +1000

    sdfsdf

--- src/test/java/io/chaosity/codes/algs/MTrieTest.java
+++ src/test/java/io/chaosity/codes/algs/MTrieTest.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
@@ -37,25 +39,297 @@ class MTrieTest {
 	}
 
 	@Test
-	void bribeStuff() {
-//		List<Integer> list = Arrays.asList(1, 2, 3, 5, 4, 6, 7, 8);
-//		List<Integer> list = Arrays.asList(4, 1, 2, 3);
-//		List<Integer> list = Arrays.asList(2, 1, 5, 3, 4);
-		List<Integer> list = Arrays.asList(1, 2, 5, 3, 7, 8, 6, 4);
-		Integer[] arr = list.toArray(new Integer[0]);
-		int bribes = 0;
-		for (int i = 0; i < arr.length; i++) {
-			int diff = arr[i] - i - 1;
-			if (diff > 2) {
-				System.out.println("Too chaotic");
-				return;
+	void abbreviation() {
+
+		String a = "daBcd";
+		String b = "ABC";
+		if (a.length() < b.length()) {
+			System.out.println("NO");
+		}
+
+		boolean[][] dp = new boolean[a.length() + 1][b.length() + 1];
+
+		for (int i = 0; i <= a.length(); i++) {
+			for (int j = 0; j <= b.length(); j++) {
+				if (j > i) {
+					dp[i][j] = false;
+				} else if (i == 0 && j == 0) {
+					dp[i][j] = true;
+				} else if (j == 0) {
+					dp[i][j] = !Character.isUpperCase(a.charAt(i - 1)) && dp[i - 1][j];
+				} else if (Character.isUpperCase(a.charAt(i - 1))) {
+					boolean tmp = a.charAt(i - 1) == b.charAt(j - 1) && dp[i - 1][j - 1];
+					dp[i][j] = tmp;
+				} else {
+					boolean tmp = (Character.toUpperCase(a.charAt(i - 1)) == b.charAt(j - 1)) && dp[i - 1][j - 1];
+					boolean tmp2 = (tmp || dp[i - 1][j]);
+					dp[i][j] = tmp2;
+				}
+			}
+		}
+
+		String res = dp[a.length()][b.length()] ? "YES" : "NO";
+		System.out.println(res);
+	}
+
+	@Test
+	void abbreviation2() {
+
+		String a = "daBcd";
+		String b = "ABC";
+		boolean[][] isValid = new boolean[a.length() + 1][b.length() + 1];
+		isValid[0][0] = true;
+		for (int i = 1; i <= a.length(); i++) {
+			if (Character.isUpperCase(a.charAt(i - 1))) {
+				isValid[i][0] = false;
+			} else
+				isValid[i][0] = true;
+		}
+		for (int i = 1; i <= a.length(); i++) {
+			for (int j = 1; j <= b.length(); j++) {
+				if (a.charAt(i - 1) == b.charAt(j - 1)) {
+					isValid[i][j] = isValid[i - 1][j - 1];
+				} else if (Character.toUpperCase(a.charAt(i - 1)) == b.charAt(j - 1)) {
+					isValid[i][j] = isValid[i - 1][j - 1] || isValid[i - 1][j];
+				} else if (Character.isUpperCase(a.charAt(i - 1))) {
+					isValid[i][j] = false;
+				} else {
+					isValid[i][j] = isValid[i - 1][j];
+				}
+			}
+		}
+
+		String res = isValid[a.length()][b.length()] ? "YES" : "NO";
+		System.out.println(res);
+	}
+
+	@Test
+	void countTriplet() {
+//		List<Long> arr = Arrays.asList(1l, 4l, 16l, 64l);
+		List<Long> arr = Arrays.asList(1l, 3l, 9l, 9l, 27l, 81l);
+		int r = 3;
+		Map<Long, Long> nextMap = new HashMap<>();
+		for (long item : arr)
+			nextMap.put(item, nextMap.getOrDefault(item, 0l) + 1);
+
+		Map<Long, Long> prevMap = new HashMap<>();
+		long sum = 0;
+		for (long item : arr) {
+
+			long prev_count = 0;
+			long next_count = 0;
+
+			nextMap.put(item, nextMap.getOrDefault(item, 0l) - 1);
+			if (prevMap.containsKey(item / r) && item % r == 0) {
+				prev_count = prevMap.get(item / r);
 			}
 
-			bribes += diff < 0 ? 0 : diff;
+			if (nextMap.containsKey(item * r)) {
+				next_count = nextMap.get(item * r);
+			}
+
+			sum += prev_count * next_count;
+
+			prevMap.put(item, prevMap.getOrDefault(item, 0l) + 1);
+
+		}
+
+		System.out.println(sum);
+
+	}
+
+	@Test
+	void SherlockAndAnagram() {
+
+		String s = "abba";
+
+		Map<String, Integer> map = new HashMap<>();
+		for (int i = 0; i < s.length(); i++) {
+			for (int j = i; j < s.length(); j++) {
+				char[] arr = s.substring(i, j + 1).toCharArray();
+				Arrays.sort(arr);
+				String str = new String(arr);
+				map.put(str, map.containsKey(str) ? map.get(str) + 1 : 1);
+			}
+		}
+
+		int result = 0;
+		for (String str : map.keySet()) {
+			int occurances = map.get(str);
+			result += occurances * (occurances - 1) / 2;
+		}
+
+		System.out.println(map);
+		System.out.println(result);
+
+	}
+
+	@Test
+	void bribeStuff() {
+//		int[] arr = { 1, 2, 5, 3, 7, 8, 6, 4 };// list.toArray(new Integer[0]);
+		int[] arr = { 1, 2, 3, 5, 7, 8, 6, 4 };
+//		int[] init = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+		int bribes = 0;
+		for (int i = arr.length - 1; i >= 0; i--) {
+			if (arr[i] != i + 1) {
+				if (i - 1 >= 0 && arr[i - 1] == i + 1) {
+					bribes++;
+					swap(arr, i, i - 1);
+				} else if (i - 1 >= 0 && arr[i - 2] == i + 1) {
+					bribes += 2;
+					swap(arr, i - 2, i - 1);
+					swap(arr, i - 1, i);
+				} else {
+					System.out.println("Too chaotic");
+					return;
+				}
+
+			}
 		}
 
 		System.out.println(bribes);
 
+	}
+
+	@Test
+	void minimumSwap() {
+//		int[] arr = { 7, 1, 3, 2, 4, 5, 6 };
+//		int[] arr = { 4, 3, 1, 2 };
+		int[] arr = { 1, 3, 5, 2, 4, 7 };
+		int swaps = 0;
+		for (int i = 0; i < arr.length; i++) {
+			int minIndex = i;
+			for (int j = i; j < arr.length; j++)
+				if (arr[j] < arr[minIndex])
+					minIndex = j;
+			if (i != minIndex) {
+				swaps++;
+				swap(arr, minIndex, i);
+			}
+//			if (sorted)
+//				break;
+		}
+		System.out.println(swaps);
+		System.out.println(Arrays.toString(arr));
+	}
+
+	@Test
+	void minimumSwap2() {
+//		int[] arr = { 7, 1, 3, 2, 4, 5, 6 };
+		int[] arr = { 4, 3, 1, 2 };
+//		int[] arr = { 1, 3, 5, 2, 4, 7 };
+//		int swaps = sort(arr, 0, arr.length - 1);
+
+		int swaps = 0;
+
+		int i = 0;
+		while (i < arr.length) {
+			if (arr[i] != i + 1) {
+				if (arr[i] - 1 < arr.length) {
+					var temp = arr[arr[i] - 1];
+					arr[arr[i] - 1] = arr[i];
+					arr[i] = temp;
+					swaps++;
+					i--;
+				}
+			}
+			i++;
+		}
+		System.out.println(swaps);
+		System.out.println(Arrays.toString(arr));
+	}
+
+	@Test
+	void maxArraySum() {
+		int max = maxArraySumSol();
+
+	}
+
+	private int maxArraySumSol() {
+		int[] arr = new int[10];
+		int size = arr.length;
+		if (size == 0 || size == 2)
+			return 0;
+		if (size == 1)
+			return arr[1];
+
+		int[] dp = new int[size];
+		dp[0] = Math.max(Integer.MIN_VALUE, arr[0]);
+		dp[1] = Math.max(dp[0], arr[1]);
+
+		for (int i = 2; i < size; i++) {
+			dp[i] = Math.max(dp[i - 1], arr[i] + dp[i - 2]);
+		}
+
+		return dp[size - 1];
+
+	}
+
+	@Test
+	void arrayManipulation() {
+		List<List<Integer>> list = new ArrayList<>();
+		int n = 10;
+		long[] arr = new long[n];
+		for (List<Integer> operation : list) {
+			Integer[] query = operation.toArray(new Integer[0]);
+			int start = query[0] - 1;// a - 1;
+			int end = query[1] - 1;// b - 1;
+			long v = query[2];
+			arr[start] += v;
+			if (end + 1 < arr.length)
+				arr[end + 1] -= v;
+		}
+		long sum = 0;
+		long max = 0;
+		for (int i = 0; i < n; i++) {
+			sum += arr[i];
+			if (sum > max)
+				max = sum;
+		}
+		System.out.println(Arrays.toString(arr));
+		System.out.println(max);
+	}
+
+	@Test
+	void commonSubString() {
+		String a = "hackerrankcommunity";
+		String b = "cdecdecdecde";
+		char[] aChars = a.toCharArray();
+		char[] bChars = b.toCharArray();
+		int i = 0;
+		boolean[] ref = new boolean[26];
+		for (int k = 0; k < aChars.length; k++)
+			ref[aChars[k] - 'a'] = true;
+		int j = 0;
+		while (j < bChars.length) {
+			if (ref[aChars[i] - 'a']) {
+				System.out.println("YES");
+				break;
+			}
+			j++;
+		}
+		System.out.println("NO");
+	}
+
+	private int sort(int[] arr, int start, int end) {
+		if (start >= end)
+			return 0;
+		int swaps = 0;
+		int boundry = start - 1;
+		int pivot = arr[end];
+		for (int i = start; i <= end; i++) {
+			if (arr[i] <= pivot) {
+				boundry++;
+				if (boundry != i) {
+					swaps++;
+					swap(arr, i, boundry);
+				}
+			}
+		}
+		sort(arr, start, boundry - 1);
+		sort(arr, boundry + 1, end);
+		return swaps;
 	}
 
 	@Test
@@ -79,9 +353,9 @@ class MTrieTest {
 	}
 
 	private void swap(int[] arr, int first, int second) {
-		var temp = arr[first];
-		arr[first] = arr[second];
-		arr[second] = temp;
+		var temp = arr[second];
+		arr[second] = arr[first];
+		arr[first] = temp;
 	}
 
 	@Test
